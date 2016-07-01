96f27e3a2b5d25b87092559a1d28e45e2b5be009
Dylan Vuz <vuzdylan@gmail.com>
2016-07-01 14:35:08 -0700

    get rid of hash history

--- app/js/app.jsx
+++ app/js/app.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import routes from './routes';
-import { Router, hashHistory } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
 
 window.onload = () => {
   ReactDOM.render(
     <Provider store={store}>
-      <Router history={hashHistory}>
+      <Router history={browserHistory}>
         {routes}
       </Router>
     </Provider>,
