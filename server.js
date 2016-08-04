bdba6eea81a7575f6d5c62776ed3f31ccdaffee1
Dylan Vuz <vuzdylan@gmail.com>
2016-08-03 23:03:50 -0700

    fixed webpack router hot realoding

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
-new WebpackDevServer(webpack(config), {
+var server = new WebpackDevServer(webpack(config), {
   contentBase: './app',
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(process.env.PORT || 5000, 'localhost', function (err) {
-    if (err) {
-      console.log(err);
-    }
-    console.log('Listening at localhost:' + (process.env.PORT || 5000));
-  });
+})
+
+server.listen(process.env.PORT || 5000, 'localhost', function (err) {
+  if (err) {
+    console.log(err);
+  }
+  console.log('Listening at localhost:' + (process.env.PORT || 5000));
+});
