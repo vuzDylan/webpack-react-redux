04c562343b74253507478bf5af16b62eb49f7794
Dylan Vuz <vuzdylan@gmail.com>
2016-08-10 18:50:47 -0700

    linter works again

--- app/js/store.js
+++ app/js/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import reducer from './reducers';
 import thunk from 'redux-thunk';
+import reducer from './reducers';
 
 const store = createStore(
     reducer,
