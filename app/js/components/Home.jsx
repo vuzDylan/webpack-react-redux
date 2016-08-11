b894a5f493f951e88d39d82e8a74702688f5071d
Dylan Vuz <vuzdylan@gmail.com>
2016-08-10 17:52:49 -0700

    as class

--- app/js/components/Home.jsx
+++ app/js/components/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const Home = () => (
-  <div>Hello World</div>
-);
+class Home extends React.Component {
+  render() {
+    return (
+      <div>Hello World</div>
+    )
+  }
+}
 
 export default Home;
