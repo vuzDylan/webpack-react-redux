78ecbd594e06e7a9de94199e6f7aa78d13b4c0fe
Dylan Vuz <vuzdylan@gmail.com>
2016-08-10 17:54:54 -0700

    fix

--- app/js/components/Home.jsx
+++ app/js/components/Home.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
 
-var a
+const Home = (props) => (
+  <div>Hello World</div>
+);
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div>Hello World</div>
-    )
-  }
-}
-
-export default Home
+export default Home;
