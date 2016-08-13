491d17da92e3e352c2325cbd31a3504914e023d1
Dylan Vuz <vuzdylan@gmail.com>
2016-08-12 22:41:10 -0700

    loaders

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,15 @@ module.exports = {
         loaders: ['style', 'css', 'resolve-url', 'sass']
       },
       {
-        test: /\.(gif|png|jpg|jpeg|ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
+      },
+      {
+        test: /\.(gif|png|jpg|jpeg)(\?[a-z0-9]+)?$/,
         loader: 'url?limit=8192'
       },
     ],
